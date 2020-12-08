2fa89b3ae50bb7c3daa755583f1f00b67721508f
Bruno Bortagaray <bruno.bortagaray@justwatch.com>
2020-12-08 21:53:51 +0100

    Finalizado os conceitos de RN

--- src/index.js
+++ src/index.js
@@ -1,13 +1,61 @@
-import React from 'react';
-import {View, Text, StyleSheet, StatusBar} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {
+  SafeAreaView,
+  FlatList,
+  Text,
+  StyleSheet,
+  StatusBar,
+  TouchableOpacity,
+} from 'react-native';
+
+import api from './services/api';
 
 export default function App() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get('projects').then((response) => {
+      console.log(response.data);
+      setProjects(response.data);
+    });
+  }, []);
+
+  async function handleAddProject() {
+    const response = await api.post('projects', {
+      title: `Novo projeto ${Date.now()}`,
+      owner: 'Bruno Bortagaray',
+    });
+
+    const project = response.data;
+    setProjects([...projects, project]);
+  }
+
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
-      <View style={styles.container}>
-        <Text style={styles.title}>Hello GoStack</Text>
-      </View>
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          style={styles.container}
+          data={projects}
+          keyExtractor={(project) => project.id}
+          renderItem={({item: project}) => (
+            <Text style={styles.project}>{project.title}</Text>
+          )}
+        />
+        <TouchableOpacity
+          activeOpacity={0.6}
+          style={styles.button}
+          onPress={handleAddProject}>
+          <Text>Adicionar projeto</Text>
+        </TouchableOpacity>
+      </SafeAreaView>
+      {/* <View style={styles.container}>
+        {projects.map((project) => (
+          <Text style={styles.project} key={project.id}>
+            {project.title}
+          </Text>
+        ))}
+      </View> */}
     </>
   );
 }
@@ -16,12 +64,21 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#7159c1',
+  },
+  project: {
+    color: '#fff',
+    fontSize: 20,
+  },
+  button: {
+    backgroundColor: '#fff',
+    margin: 20,
+    height: 50,
+    borderRadius: 4,
     justifyContent: 'center',
     alignItems: 'center',
   },
-  title: {
-    color: '#fff',
-    fontSize: 32,
+  buttonText: {
     fontWeight: 'bold',
+    fontSize: 16,
   },
 });
